1ed65a43e1bdd271cb5f764a28afc7e5a22cec7b
lxf8888 <liuxiaofeng6683@163.com>
2025-05-01 21:11:22 +0800

    [feat] 统一返回结果类型

---
+++ src/main/java/org/example/controller/Result.java
@@ -0,0 +1,43 @@
+package org.example.controller;
+
+public class Result {
+    private Object data;
+    private String message;
+    private Integer code;
+
+
+    public Result(Integer code, Object data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public Result(Integer code,Object data, String message) {
+        this.data = data;
+        this.message = message;
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
