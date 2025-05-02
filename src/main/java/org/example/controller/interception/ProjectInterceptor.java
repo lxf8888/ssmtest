75715ce42d46ec5a78a55cbc5beba1ad9c2662d8
lxf8888 <liuxiaofeng6683@163.com>
2025-05-02 15:29:26 +0800

    [feat] 新增拦截器参数相关内容

--- src/main/java/org/example/controller/interception/ProjectInterceptor.java
+++ src/main/java/org/example/controller/interception/ProjectInterceptor.java
@@ -2,6 +2,7 @@ package org.example.controller.interception;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,19 +14,30 @@ public class ProjectInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("preHandle");
+        String uri = request.getRequestURI();
+        System.out.println("uri"+uri);
+//        打印handler
+        System.out.println("handler"+handler);
+//        查看handler的类型
+        System.out.println("getClass"+handler.getClass());
+//        强转handler，获取原始方法
+        HandlerMethod hm = (HandlerMethod) handler;
+        System.out.println("hm"+hm);
+
 //        return false 终止原始操作
         return true;
-//        return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         System.out.println("postHandle");
+//      modelAndView 封装了页面跳转的相关内容
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         System.out.println("afterCompletion");
+//       ex 包含执行过程中的ex，不过已经被异常处理单独捕获，一般没用
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 
